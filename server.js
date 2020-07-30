433e30ddeb01ab446f6fe48c83f78b11d0fabe13
melanie <melanieduah@gmail.com>
2020-07-30 01:04:34 -0500

    mongo url fix

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(express.json());
 app.use(express.static("public"));
 
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workoutdb", {
+mongoose.connect(process.env.MONGODB_URL || "mongodb://localhost/workoutdb", {
   useNewUrlParser: true,
   useFindAndModify: false
 });
